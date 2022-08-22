64bce96eedf9086c0d6d149e2e3a0d9841ab66b0
bionicop <ujjwalsidhu123@gmail.com>
2022-08-22 10:57:09 +0530

    comment

--- calculator.java
+++ calculator.java
@@ -8,7 +8,7 @@ public class calculator {
         Scanner grasp = new Scanner(System.in);
         do
         {
-            System.out.println("Enter first number: "); //xd
+            System.out.println("Enter first number: "); //this is a first number
             a = grasp.nextInt();
             System.out.println("Enter second number: ");
             b = grasp.nextInt();
