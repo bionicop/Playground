cae74f63d275f76cc2b62934fae83c07f246c71a
bionicop <ujjwalsidhu123@gmail.com>
2022-12-10 16:04:18 +0530

    a small game thanks to https://github.com/mortennobel/cpp-cheatsheet for cheatsheet

---
+++ c++/guess_the_number.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>  // for rand() and srand()
+#include <ctime>    // for time()
+
+int main() {
+  srand(time(0)); // Help's in the generation of random numbers by seeding rand with a starting value. source: https://stackoverflow.com/questions/4736485/srandtime0-and-random-number-generation
+
+  int score = 0, round = 3,tries = 0;
+  std::cout << "=== Welcome to GUeSSSSSSSS GAME ===\n\n";
+
+  do {
+    int target = rand() % 11;
+
+    std::cout << "GuesS the number between 0 - 10: ";
+    int guess;
+    std::cin >> guess;
+
+    // Check's whether the player guess is correct.
+    if (guess == target) {
+      std::cout << "\nCoRRReakt!!! You guessed the right number.\n\n";
+      score++;
+    } else {
+      std::cout << "\n are you serious right NEOW bro InCoRRReakt!!! the right number was " << target << ".\n\n";
+    }
+    tries++;
+  } while (score < round);
+  std::cout << "Your final score is " << score << "/"<< round << "\n";
+  std::cout << "ANDDDDDDDDDDDDDDDDDDDDD It took you only" << tries << " tries to win the game.\n";
+  std::cout << "Congratulations you have won!\n";
+
+  return 0;
+}
